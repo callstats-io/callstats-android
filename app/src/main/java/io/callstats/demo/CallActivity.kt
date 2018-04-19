cedfb72ffbca3e88a1d642c32dddf8392fa8ef3d
Amornchai Kanokpullwad <zoonref@gmail.com>
2018-04-19 15:28:05 +0700

    add signaling

--- app/src/main/java/io/callstats/demo/CallActivity.kt
+++ app/src/main/java/io/callstats/demo/CallActivity.kt
@@ -2,11 +2,45 @@ package io.callstats.demo
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import io.callstats.demo.csiortc.CsioSignaling
 
 class CallActivity : AppCompatActivity() {
+
+  companion object {
+    const val EXTRA_ROOM = "extra_room"
+    const val EXTRA_USER = "extra_user"
+  }
+
+  private var signaling: CsioSignaling? = null
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_call)
+
+    val room = intent.getStringExtra(EXTRA_ROOM) ?: throw IllegalArgumentException("need room")
+    val user = intent.getStringExtra(EXTRA_USER) ?: throw IllegalArgumentException("need user")
+    start(room, user)
+  }
+
+  fun start(room: String, user: String) {
+    signaling = CsioSignaling(user, object : CsioSignaling.Callback {
+      override fun onConnect() {
+        signaling?.start(room)
+      }
+
+      override fun onConnectError() {
+
+      }
+
+      override fun onPeerJoin(peerId: String) {
+      }
+
+      override fun onPeerLeave(peerId: String) {
+      }
+
+      override fun onMessage(fromId: String, message: String) {
+      }
+
+    })
   }
 }
--- app/src/main/java/io/callstats/demo/MainActivity.kt
+++ app/src/main/java/io/callstats/demo/MainActivity.kt
@@ -3,6 +3,8 @@ package io.callstats.demo
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -10,6 +12,20 @@ class MainActivity : AppCompatActivity() {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_main)
 
-    startActivity(Intent(this, CallActivity::class.java))
+    submit.setOnClickListener {
+      val room = room.text.toString()
+      val user = username.text.toString()
+
+      // check the inputs
+      if (room.isBlank() || user.isBlank()) {
+        Toast.makeText(this, R.string.main_error_input, Toast.LENGTH_SHORT).show()
+        return@setOnClickListener
+      }
+
+      val intent = Intent(this, CallActivity::class.java)
+      intent.putExtra(CallActivity.EXTRA_ROOM, room)
+      intent.putExtra(CallActivity.EXTRA_USER, user)
+      startActivity(intent)
+    }
   }
 }
